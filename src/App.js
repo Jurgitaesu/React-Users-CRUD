3a4b653520a2cd67059e994a4013f477c7252de1
Jurgita Adomaviciene <jurgitasim@live.com>
2021-12-07 13:44:37 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,99 +1,95 @@
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import {useState, useEffect} from 'react';
-import Navbar from "./components/Navbar";
-import Users from "./pages/Users";
-import Form from "./pages/Form";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+import Users from './pages/Users';
+import Form from './pages/Form';
 
 function App() {
-    const [users, setUsers] = useState('');
-    const [formMessage, setFormMessage] = useState('');
-    const [updateMessage, setUpdateMessage] = useState('');
-    const [userInfo, setUserInfo] = useState('');
+  const [users, setUsers] = useState('');
+  const [formMessage, setFormMessage] = useState('');
+  const [updateMessage, setUpdateMessage] = useState('');
+  const [userInfo, setUserInfo] = useState('');
 
-    setTimeout(function () {
-        setFormMessage('');
-        setUpdateMessage('');
-    }, 4000);
+  setTimeout(function () {
+    setFormMessage('');
+    setUpdateMessage('');
+  }, 4000);
 
-    useEffect(() => {
-        async function showUsers() {
-            const response = await fetch('/show');
-            const data = await response.json();
-            setUsers(data.users);
-        }
-
-        showUsers();
-    }, [])
-
-    async function addUser(user) {
-        const options = {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        }
-        const response = await fetch('/upload', options);
-        const data = await response.json();
-        setFormMessage(data.message);
-        if (data.success) {
-            setUsers(data.users);
-        }
+  useEffect(() => {
+    async function showUsers() {
+      const response = await fetch('/show');
+      const data = await response.json();
+      setUsers(data.users);
     }
+    showUsers();
+  }, []);
 
-    async function deleteUser(id) {
-        const response = await fetch(`/delete/${id}`);
-        const data = await response.json();
-        setUpdateMessage(data.message);
-        setUsers(data.users);
+  async function addUser(user) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    };
+    const response = await fetch('/upload', options);
+    const data = await response.json();
+    setFormMessage(data.message);
+    if (data.success) {
+      setUsers(data.users);
     }
+  }
 
-    async function findUser(id) {
-        const response = await fetch(`/user/${id}`);
-        const data = await response.json();
-        setUserInfo(data.userInfo[0]);
+  async function deleteUser(id) {
+    const response = await fetch(`/delete/${id}`);
+    const data = await response.json();
+    setUpdateMessage(data.message);
+    setUsers(data.users);
+  }
+
+  async function findUser(id) {
+    const response = await fetch(`/user/${id}`);
+    const data = await response.json();
+    setUserInfo(data.userInfo[0]);
+  }
+
+  async function updateUser(user) {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    };
+    const response = await fetch('/update', options);
+    const data = await response.json();
+    setUpdateMessage(data.message);
+    setUserInfo('');
+    if (data.success) {
+      setUsers(data.users);
     }
+  }
 
-    async function updateUser(user) {
-        const options = {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        }
-        const response = await fetch('/update', options);
-        const data = await response.json();
-        setUpdateMessage(data.message);
-        setUserInfo('');
-        if (data.success) {
-            setUsers(data.users);
-        }
-    }
-
-    return (
-        <Router>
-            <Navbar/>
-            <Switch>
-                <Route exact path="/">
-                    <Users
-                        updateMessage={updateMessage}
-                        users={users}
-                        deleteId={deleteUser}
-                        findUser={findUser}
-                        userInfo={userInfo}
-                        updateUser={updateUser}
-                    />
-                </Route>
-                <Route path="/form">
-                    <Form
-                        formMessage={formMessage}
-                        addUser={addUser}
-                    />
-                </Route>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Users
+            updateMessage={updateMessage}
+            users={users}
+            deleteId={deleteUser}
+            findUser={findUser}
+            userInfo={userInfo}
+            updateUser={updateUser}
+          />
+        </Route>
+        <Route path="/form">
+          <Form formMessage={formMessage} addUser={addUser} />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
